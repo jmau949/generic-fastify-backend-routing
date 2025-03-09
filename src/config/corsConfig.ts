2b07fe1e45dd1c3c8d897d15b78105f8841ff637
jmau949 <44676775+jmau949@users.noreply.github.com>
2025-03-09 13:19:44 -0500

    Update corsConfig.ts

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -6,7 +6,12 @@ const corsConfig = {
     allowedHeaders: ["Content-Type", "Authorization"],
   },
   production: {
-    origin: ["https://jonathanmau.com", "https://www.jonathanmau.com"],
+    origin: [
+      "https://jonathanmau.com",
+      "https://www.jonathanmau.com",
+      "https://ai.jonathanmau.com",
+      "https://www.ai.jonathanmau.com",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Added OPTIONS for preflight
     credentials: true, // Enable credentials for cookies
     allowedHeaders: ["Content-Type", "Authorization"],
